7ce4223fc7db24a3842b6177871b75cfab9b638d
Michal Paszowski <m.paszowski@livechatinc.com>
2019-08-28 13:27:44 +0200

    Add suport of `bubbleStyles`

--- LiveChat/LiveChat.js
+++ LiveChat/LiveChat.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import { View, Dimensions, StyleSheet, Image, Platform } from 'react-native';
+import { View, Dimensions, StyleSheet, Image } from 'react-native';
 import PropTypes from 'prop-types';
-import ChatBubble from './ChatBubble/ChatBubble';
-import Chat from './Chat/Chat';
-import {init} from "@livechat/livechat-visitor-sdk";
+import ChatBubble from './components/ChatBubble';
+import Chat from './components/Chat';
+GLOBAL.cookie = '';
+import { init } from "./sdk/livechat-visitor-sdk.min";
 
-const chatIcon = require('../assets/chat.png');
-
-const { height, width } = Dimensions.get('window');
+const chatIcon = require('./assets/chat.png');
+const { width } = Dimensions.get('window');
 
 export default class LiveChat extends Component {
   constructor(props) {
     super(props);
+    GLOBAL.cookie = '';
     this.defineStyles();
+
     this.state = {
       isChatOn: false,
       bubble: props.bubble ? props.bubble : (
@@ -21,33 +23,34 @@ export default class LiveChat extends Component {
         </View>
       ),
     };
-    if (!GLOBAL.visitorSDK) {
-      GLOBAL.visitorSDK = init({
-        license: props.license,
-        group: props.group
-      });
-    }
+
+    GLOBAL.visitorSDK = init({
+      license: props.license,
+      group: props.group,
+    });
+
     props.onLoaded(GLOBAL.visitorSDK);
   }
 
-  defineStyles() {
+  defineStyles = () => {
     this.styles = StyleSheet.create({
       bubbleStyle: {
-        width: width / 5,
-        height: width / 5,
+        width: width / 6,
+        height: width / 6,
         backgroundColor: this.props.bubbleColor,
-        borderRadius: width / 10,
+        borderRadius: width / 12,
         alignItems: 'center',
         justifyContent: 'center',
       },
       icon: {
-        width: width / 7, height: width / 7,
+        width: width / 10, height: width / 10,
       },
-      container: {
-        position: 'absolute',
-      },
+      chatBubbleStyles: {
+        bottom: this.props.bubbleBottom || 12,
+        right: this.props.bubbleRight || 12,
+      }
     });
-  }
+  };
 
   openChat = () => {
     this.setState({ isChatOn: true });
@@ -58,24 +61,28 @@ export default class LiveChat extends Component {
   };
 
   render() {
-    return (
-      <View style={this.styles.container}>
-        <ChatBubble
-          left={this.props.bubbleLeft}
-          top={this.props.bubbleTop}
-          openChat={this.openChat}
-          bubble={this.state.bubble}
-          disabled={this.props.movable}
-        />
-        <Chat {...this.props} isChatOn={this.state.isChatOn} closeChat={this.closeChat} />
-      </View>
-    );
+    const { isChatOn } = this.state;
+
+    return [
+      <ChatBubble
+        key="bubble"
+        openChat={this.openChat}
+        bubble={this.state.bubble}
+        disabled={this.props.movable}
+        styles={this.props.bubbleStyles}
+      />,
+      <Chat
+        key="chat"
+        {...this.props}
+        isChatOn={isChatOn}
+        closeChat={this.closeChat}
+      />
+    ];
   }
 }
 
 LiveChat.propTypes = {
-  license: PropTypes.number.isRequired,
-  group: PropTypes.number,
+  license: PropTypes.string.isRequired,
   movable: PropTypes.bool,
   bubble: PropTypes.element,
   bubbleColor: PropTypes.string,
@@ -85,15 +92,21 @@ LiveChat.propTypes = {
   greeting: PropTypes.string,
   noAgents: PropTypes.string,
   onLoaded: PropTypes.func,
+  styles: PropTypes.object
 };
 
 LiveChat.defaultProps = {
-  bubbleColor: '#2196F3',
+  bubbleColor: '#2962FF',
   movable: true,
-  onLoaded: () => {},
-  bubbleLeft: width - (width / 5) - (width / 50),
-  bubbleTop: Platform.OS === 'ios' ? height - (width / 5) - (width / 50) : height - (width / 5) - (width / 13),
+  onLoaded: () => {
+  },
+  group: 0,
   chatTitle: 'Chat with us!',
   greeting: 'Welcome to our LiveChat!\nHow may We help you?',
   noAgents: 'Our agents are not available right now.',
+  bubbleStyles: {
+    position: "absolute",
+    bottom: 12,
+    right: 12,
+  }
 };
